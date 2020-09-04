78169e0e8ac852d7aa7fc8278a8c4a73819153bc
charith <charithcherry1100gmail.com>
2020-09-04 16:26:56 +0530

    prep for deploy

--- js/scripts.js
+++ js/scripts.js
@@ -11,9 +11,8 @@ $(document).ready(function(){
        $('#carouselButton').children('span').removeClass('fa-play');
        $('#carouselButton').children('span').addClass('fa-pause');
 
-       }
+       }});
        
-});
 $('#loginButton').click(function() {
         $('#loginModal').modal('show');
       });
